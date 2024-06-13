6b8ec5b5fd09b32cb95dcaf02e96abaca6974a0e
evajungabel <eva.jungabel@gmail.com>
2024-06-14 00:45:41 +0200

    changes

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -1,34 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-// script.js
     fetch('navbar.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('navbar-placeholder').innerHTML = data;
-            // addClickEventToLinks();  // Call the function after the navbar is loaded
         })
         .catch(error => console.error('Error fetching the navbar:', error));
-
-    // function addClickEventToLinks() {
-    //     const navLinks = document.querySelectorAll('#navbar-placeholder .nav-a');
-    //
-    //     navLinks.forEach(link => {
-    //         link.addEventListener('click', function(event) {
-    //             // Prevent default action to allow for single-page applications
-    //             event.preventDefault();
-    //
-    //             // Remove the underlined class from all links
-    //             navLinks.forEach(navLink => navLink.classList.remove('underlined'));
-    //
-    //             // Add the underlined class to the clicked link
-    //             this.classList.add('underlined');
-    //
-    //             console.log('jjj');
-    //             // Optionally navigate to the new page
-    //             window.location.href = this.href;
-    //         });
-    //     });
-    // }
 
 });
 
